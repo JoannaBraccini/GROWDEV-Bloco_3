137049f3a550b39614ddbd35271c3393c45a6119
Joanna <joannabraccini@gmail.com>
2024-12-20 17:36:11 -0300

    limpeza e alertas

--- HarryPotter/src/store/modules/characters/charactersThunk.ts
+++ HarryPotter/src/store/modules/characters/charactersThunk.ts
@@ -1,16 +1,23 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { api } from "../../../configs/services/api.service";
 import { Character } from "./charactersTypes";
+import { showAlert } from "../alert/AlertSlice";
 
 export const fetchCharactersThunk = createAsyncThunk<Character[]>(
   "characters/fetchAll",
-  async () => {
+  async (_, { dispatch }) => {
     try {
       const response = await api.get("/characters");
 
-      if (response.data) {
-        return response.data;
+      if (!response.data) {
+        dispatch(
+          showAlert({
+            message: "Erro ao buscar dados da API",
+            type: "error",
+          })
+        );
       }
+      return response.data;
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
       return error.response.message;
